2319c70cbb3de08310969755be207039992c5eac
eduardo.anjos <eduardo.h.anjos0411@gmail.com>
2025-06-23 10:55:17 -0300

    Creating UserService class to implement logic

---
+++ src/main/java/com/dudu/HelpDeskAPI/service/UserService.java
@@ -0,0 +1,54 @@
+package com.dudu.HelpDeskAPI.service;
+
+import com.dudu.HelpDeskAPI.dto.UserRequest;
+import com.dudu.HelpDeskAPI.dto.UserResponse;
+import com.dudu.HelpDeskAPI.model.User;
+import com.dudu.HelpDeskAPI.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    public final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public UserResponse createNewUser(UserRequest dataToCreate) {
+        User newUser = new User();
+        newUser.setNome(dataToCreate.nome());
+        newUser.setEmail(dataToCreate.email());
+        newUser.setSenha(dataToCreate.senha());
+        userRepository.save(newUser);
+
+        return new UserResponse(dataToCreate.nome(), dataToCreate.email());
+    }
+
+    public List<UserResponse> getAllUsers() {
+        return userRepository.findAll().stream().map(UserResponse::fromEntity).toList();
+    }
+
+    public UserResponse getUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+
+        return new UserResponse(user.getNome(), user.getEmail());
+    }
+
+    public UserResponse updateUser(Long id, UserRequest dataToUpdate) {
+        User userFound = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        userFound.setNome(dataToUpdate.nome());
+        userFound.setEmail(dataToUpdate.email());
+        userFound.setSenha(dataToUpdate.senha());
+        userRepository.save(userFound);
+
+        return new UserResponse(dataToUpdate.nome(), dataToUpdate.email());
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
