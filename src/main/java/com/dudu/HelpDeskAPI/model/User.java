e4fa9c297f7a0805087160ffe8a0b24a55534677
eduardo.anjos <eduardo.h.anjos0411@gmail.com>
2025-06-23 11:32:48 -0300

    Optimizing class structure

--- src/main/java/com/dudu/HelpDeskAPI/model/User.java
+++ src/main/java/com/dudu/HelpDeskAPI/model/User.java
@@ -1,10 +1,7 @@
 package com.dudu.HelpDeskAPI.model;
 
 import com.dudu.HelpDeskAPI.enums.Perfil;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +16,8 @@ public class User {
     private String nome;
     private String email;
     private String senha;
+
+    @Enumerated(EnumType.STRING)
     private Perfil perfil;
 
 
