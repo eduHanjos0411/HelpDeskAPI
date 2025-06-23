60ba14c529087bb4ec9b14dc1e33450669cfb8d1
eduardo.anjos <eduardo.h.anjos0411@gmail.com>
2025-06-23 08:53:55 -0300

    Creating User entity

---
+++ src/main/java/com/dudu/HelpDeskAPI/model/User.java
@@ -0,0 +1,24 @@
+package com.dudu.HelpDeskAPI.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+    private Perfil perfil;
+
+
+}
