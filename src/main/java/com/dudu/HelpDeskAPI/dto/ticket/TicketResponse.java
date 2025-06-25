87660d9b085a39a07ff474808d3f346f740f88cd
eduardo.anjos <eduardo.h.anjos0411@gmail.com>
2025-06-25 13:26:22 -0300

    adding new attributes to attend to business rules

--- src/main/java/com/dudu/HelpDeskAPI/dto/ticket/TicketResponse.java
+++ src/main/java/com/dudu/HelpDeskAPI/dto/ticket/TicketResponse.java
@@ -4,24 +4,29 @@ import com.dudu.HelpDeskAPI.enums.Categoria;
 import com.dudu.HelpDeskAPI.enums.Prioridade;
 import com.dudu.HelpDeskAPI.model.Ticket;
 import com.dudu.HelpDeskAPI.model.User;
-import lombok.Builder;
 
-@Builder
+import java.time.LocalDateTime;
+
+
 public record TicketResponse(
         String titulo,
         String descricao,
         Categoria categoria,
         Prioridade prioridade,
-        User solicitante
+        User solicitante,
+        LocalDateTime criadoEm,
+        LocalDateTime atualizadoEm
 ) {
 
-    public static TicketResponse fromEntity(Ticket ticket) {
-        return TicketResponse.builder()
-                .titulo(ticket.getTitulo())
-                .descricao(ticket.getDescricao())
-                .categoria(ticket.getCategoria())
-                .prioridade(ticket.getPrioridade())
-                .solicitante(ticket.getSolicitante())
-                .build();
+    public TicketResponse(Ticket ticket) {
+        this(
+                ticket.getTitulo(),
+                ticket.getDescricao(),
+                ticket.getCategoria(),
+                ticket.getPrioridade(),
+                ticket.getSolicitante(),
+                ticket.getCriadoEm(),
+                ticket.getAtualizadoEm()
+        );
     }
 }
