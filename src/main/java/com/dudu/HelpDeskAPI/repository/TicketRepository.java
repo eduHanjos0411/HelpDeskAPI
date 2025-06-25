99b8b1507406223effaa72a3440a19cdaa6c01b6
eduardo.anjos <eduardo.h.anjos0411@gmail.com>
2025-06-25 13:27:00 -0300

    new method to find tickets by user id wich opened them

--- src/main/java/com/dudu/HelpDeskAPI/repository/TicketRepository.java
+++ src/main/java/com/dudu/HelpDeskAPI/repository/TicketRepository.java
@@ -5,9 +5,10 @@ import com.dudu.HelpDeskAPI.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface TicketRepository extends JpaRepository<Ticket, UUID> {
-    User findById(Long id);
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findBySolicitanteId(Long id);
 }
