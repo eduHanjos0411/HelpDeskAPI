d8e4eaba77b93fe891b7a5ff7fe975c4253e1c73
eduardo.anjos <eduardo.h.anjos0411@gmail.com>
2025-06-25 13:25:43 -0300

    finishing ticket CRUDs logic

--- src/main/java/com/dudu/HelpDeskAPI/service/TicketService.java
+++ src/main/java/com/dudu/HelpDeskAPI/service/TicketService.java
@@ -2,15 +2,13 @@ package com.dudu.HelpDeskAPI.service;
 
 import com.dudu.HelpDeskAPI.dto.ticket.TicketRequest;
 import com.dudu.HelpDeskAPI.dto.ticket.TicketResponse;
-import com.dudu.HelpDeskAPI.dto.user.UserResponse;
+import com.dudu.HelpDeskAPI.dto.ticket.TicketPutRequest;
 import com.dudu.HelpDeskAPI.enums.Status;
 import com.dudu.HelpDeskAPI.model.Ticket;
-import com.dudu.HelpDeskAPI.model.User;
 import com.dudu.HelpDeskAPI.repository.TicketRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class TicketService {
@@ -29,25 +27,49 @@ public class TicketService {
         newTicket.setCategoria(dataToCreateTicket.categoria());
         newTicket.setPrioridade(dataToCreateTicket.prioridade());
         newTicket.setSolicitante(dataToCreateTicket.solicitante());
+
         ticketRepository.save(newTicket);
 
-        return new TicketResponse(dataToCreateTicket.titulo(), dataToCreateTicket.descricao(),
-                dataToCreateTicket.categoria(), dataToCreateTicket.prioridade(), dataToCreateTicket.solicitante());
+        return new TicketResponse(newTicket);
     }
 
     public List<TicketResponse> getAllTickets() {
-        return ticketRepository.findAll().stream().map(TicketResponse::fromEntity).toList();
+        return ticketRepository.findAll().stream().map(TicketResponse::new).toList();
     }
 
-    public TicketResponse getTicketById(UUID id) {
-        Ticket ticketFound = ticketRepository.findById(id).orElseThrow(() -> new RuntimeException("Ticket not found"));
+    public TicketResponse getTicketById(Long id) {
+        Ticket ticketFound = ticketRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ticket not Found"));
 
         return new TicketResponse(ticketFound.getTitulo(), ticketFound.getDescricao(), ticketFound.getCategoria(),
-                ticketFound.getPrioridade(), ticketFound.getSolicitante());
+                ticketFound.getPrioridade(), ticketFound.getSolicitante(), ticketFound.getCriadoEm(),
+                ticketFound.getAtualizadoEm());
     }
 
-    public List<TicketResponse> getTicketByUserId(Long id) {
-        User ticketOfUserFound = ticketRepository.findById(id);
-        for
+    public List<TicketResponse> getTicketByUserId(Long userId) {
+        return ticketRepository.findBySolicitanteId(userId).stream().map(TicketResponse::new).toList();
+    }
+
+    public TicketResponse updateTicketStatusOrTecnician(TicketPutRequest newData) {
+        Ticket ticketToUpdate = ticketRepository.findById(newData.id())
+                .orElseThrow(() -> new RuntimeException("Ticket not Found"));
+
+        ticketToUpdate.setStatus(newData.status());
+        ticketToUpdate.setResponsavel(newData.responsavel());
+
+        ticketRepository.save(ticketToUpdate);
+
+        return new TicketResponse(ticketToUpdate);
+    }
+
+    public TicketResponse updateTicketStatus( Long id,Status newStatus) {
+        Ticket toUpdateStatus = ticketRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ticket not Found"));
+
+        toUpdateStatus.setStatus(newStatus);
+
+        ticketRepository.save(toUpdateStatus);
+
+        return new TicketResponse(toUpdateStatus);
     }
 }
