7b9c447a564f0a146caba5e01b8181cf36bc9fda
eduardo.anjos <eduardo.h.anjos0411@gmail.com>
2025-06-26 11:17:56 -0300

    removing unused imports

--- src/main/java/com/dudu/HelpDeskAPI/repository/TicketRepository.java
+++ src/main/java/com/dudu/HelpDeskAPI/repository/TicketRepository.java
@@ -1,12 +1,10 @@
 package com.dudu.HelpDeskAPI.repository;
 
 import com.dudu.HelpDeskAPI.model.Ticket;
-import com.dudu.HelpDeskAPI.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
